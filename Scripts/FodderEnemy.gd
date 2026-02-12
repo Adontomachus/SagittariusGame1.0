1bf02939cf90cb6f5719809af092adbd287422ea
Adontomachus <71423063+Adontomachus@users.noreply.github.com>
2026-02-12 16:18:05 +0800

    Update 1

--- Scripts/FodderEnemy.gd
+++ Scripts/FodderEnemy.gd
@@ -1,31 +1,31 @@
 extends CharacterBody2D
 
+# TEMPORARY
+@onready var node: Node2D = $".."
 
 @onready var player: CharacterBody2D = $"../Player"
 @onready var selfEnemySprite: Sprite2D = $Sprite2D
 @onready var navAgent: NavigationAgent2D = $NavigationAgent2D
 
 #STATISTICS
-var moveSpeed = 15
+var moveSpeed = 40
 var healthPoints
 var maxHealthPoints = 80
-var navTimer
 
+# START
 func _ready():
 	healthPoints = maxHealthPoints
 	pass
 
 func _process(delta):
-	player = get_tree().get_first_node_in_group("PlayerObject")
+	# player = get_tree().get_first_node_in_group("PlayerObject")
 	if (healthPoints < 0):
 		queue_free()
 		
-#FINDING TARGET FUNCTION
-func find_target():
-	if (player):
-		navTimer = 0
-		#navPosition = player.global_position + randomPosition
-		navAgent.target_position = player.global_position
+		
+	#FINDING TARGET FUNCTION
+func find_target() -> void:
+	navAgent.target_position = player.global_position
 
 	
 	#NAVIGATION AGENT SETUP
@@ -33,9 +33,16 @@ func _navigationsetup():
 	await get_tree().physics_frame
 	if player:
 		return
-	pass
 
-
+	# CHASE PLAYER TEST
+func _physics_process(delta):
+	find_target()
+	var targetLocation = navAgent.get_next_path_position()
+	var new_velocity = global_position.direction_to(targetLocation) * moveSpeed
+	velocity = new_velocity
+	if (navAgent.avoidance_enabled):
+		navAgent.set_velocity(new_velocity)
+	move_and_slide()
 
 
 
