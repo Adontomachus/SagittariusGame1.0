87f4fd2324540b082e95067170fa7629672a37bc
Adontomachus <71423063+Adontomachus@users.noreply.github.com>
2026-03-10 16:49:12 +0800

    Minor UI additions

--- Scripts/Scene Management/MainMenuScript.gd
+++ Scripts/Scene Management/MainMenuScript.gd
@@ -1,1 +1,24 @@
+class_name MainMenuSystems
+
 extends Node2D
+
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Gameplay/GameplayScene.tscn")
+	pass # Replace with function body.
+
+func _on_story_mode_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Gameplay/GameplayScene.tscn")
+	pass # Replace with function body.
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_calibration_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed() -> void:
+	pass # Replace with function body.
