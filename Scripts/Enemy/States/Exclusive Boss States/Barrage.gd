0d33dc112952da708e8afc90fddf4f05a8b1cf04
Adontomachus <71423063+Adontomachus@users.noreply.github.com>
2026-04-29 15:08:45 +0800

    Lighting Overhaul

--- Scripts/Enemy/States/Exclusive Boss States/Barrage.gd
+++ Scripts/Enemy/States/Exclusive Boss States/Barrage.gd
@@ -1,11 +1,45 @@
-extends Node
+class_name EnemyStateBarrage
+extends EnemyState
 
+@export var after_strafing_state: EnemyState
+@export var successful_chance_to_attack: float
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# Shooting Chances
+const CHANCE_RANGE: Vector2 = Vector2(0.0, 10.0)
+var can_move = 1
+var shot_times: int = 0
 
+func enter() -> void:
+	super()
+	reposition(parent.aroundPlayerRadius)
+	
+func process_physics(delta: float) -> EnemyState:
+	if can_move == 1:
+		#strafe(parent.aroundPlayerRadius)
+		can_move = 0
+	# parent.navAgent.target_position = parent.target.global_position
+	parent.move_enemy(delta)
+	parent.repositioningTimer -= 6 * delta
+	
+	if parent.stamina <= 0:
+		return recovery_state
+	
+	# Shoot on beat
+	if (GlobalBeatSync.executeAction):
+		shot_times += 1
+		if randf_range(CHANCE_RANGE.x, CHANCE_RANGE.y) >= successful_chance_to_attack:
+			parent.shoot_projectile()
+			
+	if parent.navAgent.is_navigation_finished() and after_strafing_state:
+		return after_strafing_state
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	return null
+#func strafe(playerRadius):
+#	if parent.target:
+#		var randomPosition = Vector2(randf_range(-playerRadius,playerRadius), randf_range(-playerRadius,playerRadius))
+#		parent.navAgent.target_position = parent.target.global_position + randomPosition
+#	pass
+func reposition(playerRadius) -> void:
+	if parent.target:
+		var randomPosition = Vector2(randf_range(-playerRadius,playerRadius), randf_range(-playerRadius,playerRadius))
+		parent.navAgent.target_position = parent.target.global_position + randomPosition
--- Scripts/Miscellaneous/AreaOfEffectDamage.gd
+++ Scripts/Miscellaneous/AreaOfEffectDamage.gd
@@ -9,8 +9,10 @@ enum ProjectileSide {
 @export_category("Area of Effect Statistics")
 @export var aoe_damage: float = 25
 @export var lifetime: float
+@onready var area_indicator: AnimationPlayer = $AreaIndicator
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	area_indicator.play("PulseEffect")
 	self.area_entered.connect(func(area) -> void:
 		if area is EnemyProjectileHitbox: #(area.is_in_group("EnemyObject")):
 			area.modify_enemy_health(-aoe_damage)
