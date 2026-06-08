5974905cec6d4094238a13fb0e1c43aaf41bda3f
Gabe005 <gabr.degu@gmail.com>
2026-06-08 11:21:08 +0800

    pre merge

--- Scripts/Scene Management/MainMenuScript.gd
+++ Scripts/Scene Management/MainMenuScript.gd
@@ -19,6 +19,7 @@ func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Gameplay/GameplayScene.tscn")
 	ScalingSystemScript.reset_scaling()
 	pass # Replace with function body.
+	#test
 
 func _on_story_mode_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Gameplay/GameplayScene.tscn")
